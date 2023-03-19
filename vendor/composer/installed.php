96bdfd2085d4260d40ed32b8f04b67bb8eb039a2
Aleksei Rytikov <user.raa@gmail.com>
2023-03-19 15:01:49 +0100

    composer init
    with catfan/medoo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'chlp/telepage',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'catfan/medoo' => array(
+            'pretty_version' => 'v2.1.8',
+            'version' => '2.1.8.0',
+            'reference' => 'b49a620cf1ad0d2e90ebd9f85b82aa11271df5f4',
+            'type' => 'framework',
+            'install_path' => __DIR__ . '/../catfan/medoo',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'chlp/telepage' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
