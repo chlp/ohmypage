877bdaf376282c2571db45974457773e03ffd215
Aleksei Rytikov <user.raa@gmail.com>
2023-09-02 20:27:47 +0200

    new file structure

---
+++ services/writer/bootstrap/config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+const DB_CONFIG = [
+    'type' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'public',
+    'username' => 'root',
+    'password' => 'pass'
+];
