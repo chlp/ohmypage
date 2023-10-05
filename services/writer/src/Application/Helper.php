3809c8edd692f95e697196a7e93fbe93f9e083af
Aleksei Rytikov <user.raa@gmail.com>
2023-10-05 22:01:27 +0200

    intToOhMyChar func

--- services/writer/src/Application/Helper.php
+++ services/writer/src/Application/Helper.php
@@ -3,10 +3,30 @@ declare(strict_types=1);
 
 namespace Chlp\Telepage\Application;
 
+use Exception;
+
 class Helper
 {
     public static function isUuid(string $uuid): bool
     {
         return preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/', $uuid) === 1;
+    }
+
+    const A_CHAR_INT = 97;
+
+    /**
+     * @throws Exception
+     */
+    public static function intToOhMyChar(int $i): string
+    {
+        if ($i < 0) {
+            throw new Exception("wrong num 1 ($i)");
+        } else if ($i < 10) {
+            return ((string)$i);
+        } else if ($i > 35) {
+            throw new Exception("wrong num 2 ($i)");
+        } else {
+            return (chr($i - 10 + self::A_CHAR_INT));
+        }
     }
 }
