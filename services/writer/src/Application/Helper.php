2ab7ca86b8914ead3fbeef8ba2da9ea066a49cbe
Aleksei Rytikov <user.raa@gmail.com>
2023-10-07 10:49:08 +0200

    ohMyCharToInt func

--- services/writer/src/Application/Helper.php
+++ services/writer/src/Application/Helper.php
@@ -12,7 +12,8 @@ class Helper
         return preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/', $uuid) === 1;
     }
 
-    const A_CHAR_INT = 97;
+    const A_CHAR_CODE = 97;
+    const MAX_OHMYCHAR_INT = 35;
 
     /**
      * @throws Exception
@@ -23,10 +24,31 @@ class Helper
             throw new Exception("wrong num 1 ($i)");
         } else if ($i < 10) {
             return ((string)$i);
-        } else if ($i > 35) {
+        } else if ($i > self::MAX_OHMYCHAR_INT) {
             throw new Exception("wrong num 2 ($i)");
         } else {
-            return (chr($i - 10 + self::A_CHAR_INT));
+            return (chr($i - 10 + self::A_CHAR_CODE));
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function ohMyCharToInt(string $c): int
+    {
+        if (strlen($c) !== 1) {
+            throw new Exception("wrong char len ($c)");
+        }
+        if ((string)(int)$c === $c) {
+            return (int)$c;
+        }
+        $i = ord($c) + 10 - self::A_CHAR_CODE;
+        if ($i < 10) {
+            throw new Exception("wrong char 1 ($c)");
+        } else if ($i > self::MAX_OHMYCHAR_INT) {
+            throw new Exception("wrong char 2 ($c)");
+        } else {
+            return $i;
         }
     }
 }
